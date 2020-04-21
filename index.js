769ed29a0a6e80ff8990a84aaab2ba849b6312d0
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-04-21 20:19:26 +0600

    express-templete

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
+// const middleWare = (req, res, next) => {
+//   //   console.log("%s %s", req.url, req.method);
+//   //   next();
+//   if (req.url === "/help") {
+//     res.send("The page is blocked by admin");
+//   }
+//   next();
+// };
+// app.use(middleWare);
 
-const middleWare = (req, res, next) => {
-  //   console.log("%s %s", req.url, req.method);
-  //   next();
-  if (req.url === "/help") {
-    res.send("The page is blocked by admin");
-  }
-  next();
-};
-app.use(middleWare);
+app.get("/dynamic_view", (req, res) => {
+  res.render("dynamic", {
+    // name: "faisal rahman",
+    // url: "http://www.tutorialspoint.com",
+    user: { name: "Ayush", age: "20" },
+  });
+});
+app.get("/first_template", function (req, res) {
+  res.render("first_view");
+});
 
 app.get("/", (req, res) => {
   res.send("<h2>Welcome to express js</h2>");
@@ -20,5 +32,5 @@ app.get("/help", (req, res) => {
 });
 
 //connection to port
-const port = 4200;
+const port = 3000;
 app.listen(port, () => console.log("Running on the server", port));
