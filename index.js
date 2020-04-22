46ff519e8ef70d1bf1eb324dc22bd00ec7d7a6e3
Foyshal Rahman <foyshalrahmanseu@gmail.com>
2020-04-22 17:19:41 +0600

    finished form

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const upload = multer();
 app.set("view engine", "pug");
 app.set("views", "./views");
+
 // const middleWare = (req, res, next) => {
 //   //   console.log("%s %s", req.url, req.method);
 //   //   next();
@@ -12,25 +16,42 @@ app.set("views", "./views");
 // };
 // app.use(middleWare);
 
-app.get("/dynamic_view", (req, res) => {
-  res.render("dynamic", {
-    // name: "faisal rahman",
-    // url: "http://www.tutorialspoint.com",
-    user: { name: "Ayush", age: "20" },
-  });
-});
-app.get("/first_template", function (req, res) {
-  res.render("first_view");
-});
+// app.get("/dynamic_view", (req, res) => {
+//   res.render("dynamic", {
+//     // name: "faisal rahman",
+//     // url: "http://www.tutorialspoint.com",
+//     user: { name: "Ayush", age: "20" },
+//   });
+// });
+// app.get("/first_template", function (req, res) {
+//   res.render("first_view");
+// });
 
-app.get("/", (req, res) => {
-  res.send("<h2>Welcome to express js</h2>");
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+//form-urlencoded
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static("public"));
+
+// app.get("/", (req, res) => {
+//   res.send("<h2>Welcome to express js</h2>");
+// });
+app.get("/", function (req, res) {
+  res.render("form");
 });
 
 app.get("/help", (req, res) => {
   res.send("<h2>How Can I Help You!!!</h2>");
 });
 
+app.post("/", function (req, res) {
+  console.log(req.body);
+  res.send("received your request!");
+});
+
 //connection to port
-const port = 3000;
+const port = 4200;
 app.listen(port, () => console.log("Running on the server", port));
